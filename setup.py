d5387d0e030f58b72488314f4a89618378a19a43
Andrea Genovesi <andrea.genovesi@gmail.com>
2020-10-13 16:43:12 +0200

    (https://github.com/mas2020-python/pycryptex/issues/5) remove post install from setup.py; (https://github.com/mas2020-python/pycryptex/issues/4) update pkg to include all the dependencies into setup.py; add toml into requirements.txt

--- setup.py
+++ setup.py
@@ -9,38 +9,6 @@ This instruction inform setup tool to read our doc file and to include as a long
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
-def _post_install():
-    """
-    Post installation steps are:
-    - create $HOME/.cryptex folder and $HOME/.cryptex/tmp
-    - create a default version of pycryptex.toml
-    :return:
-    """
-    # .pycryptex folder creation
-    home = Path.home()
-    pycryptex_folder = os.path.join(home, '.pycryptex')
-    if not os.path.exists(pycryptex_folder):
-        os.mkdir(pycryptex_folder)
-
-    # .pycryptex/tmp folder creation
-    pycryptex_tmp_folder = os.path.join(home, '.pycryptex', 'tmp')
-    if not os.path.exists(pycryptex_tmp_folder):
-        os.mkdir(pycryptex_tmp_folder)
-
-    # create a default version of pycryptex.toml
-    pycryptex_config_file = os.path.join(home, '.pycryptex', 'pycryptex.toml')
-    if not os.path.exists(pycryptex_config_file):
-        with open(pycryptex_config_file, "w") as f:
-            f.write("""# Configuration file for pycryptex
-[config]
-# path to the pager application where to see decrypted file
-pager = "vim"
-# number of seconds the application will delete a file decrypted passing the s option flag
-wait_delete_time = 0
-            """)
-
-
 setup(
     name="pycryptex",
     author="mas2020",
@@ -56,6 +24,7 @@ setup(
     install_requires=[
         'click==7.1.2',
         'pycryptodome==3.9.8',
+        'toml==0.10.1',
     ],
     entry_points='''
         [console_scripts]
@@ -67,5 +36,3 @@ setup(
         "Operating System :: OS Independent",
     ],
 )
-
-_post_install()
