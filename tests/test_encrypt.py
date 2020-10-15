cd309904e7cb9335551fbacee5c9fd3754666ba8
Andrea Genovesi <andrea.genovesi@gmail.com>
2020-10-15 14:36:33 +0200

    fix to the tests execution

--- tests/test_encrypt.py
+++ tests/test_encrypt.py
@@ -1,20 +1,23 @@
+import os
 from click.testing import CliRunner
 from pycryptex.main import cli
 
 
 def test_encrypt():
     runner = CliRunner()
+    os.remove('test/secrets.txt.enc')
     result = runner.invoke(cli,
-                           ['--verbose', 'encrypt', '--pubkey', 'keys/my_key.pub','test/appway.png'])
+                           ['encrypt', '--pubkey', 'test/id_rsa.pub', 'test/secrets.txt'])
     assert result.exit_code == 0
-    print(result.exit_code)
     assert not result.exception
+    assert os.path.exists('test/secrets.txt.enc') is True
+
 
 def test_decrypt():
     runner = CliRunner()
     result = runner.invoke(cli,
-                           ['decrypt', '--privkey', '/Users/andrea.genovesi/.ssh/keys/id_rsa',
-                            '/Users/andrea.genovesi/ganttproject.log.enc'])
+                           ['decrypt', '--privkey', 'test/id_rsa',
+                            'test/secrets.txt.enc'])
     assert result.exit_code == 0
-    print(result.exit_code)
     assert not result.exception
+    assert os.path.exists('test/secrets.txt') is True
